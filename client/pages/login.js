9f6cb8bc437734f331d4bcb29ff50a45aff09569
pikachungg <pikachungg@gmail.com>
2022-09-06 16:10:31 -0400

    login page basic

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,18 +1,42 @@
 import Head from 'next/head'
-import LoginStyle from '../styles/Login.module.css'
 import Navbar from '../components/Navbar'
 
 export default function Login() {
-   return (
+
+  const handleSubmit = async (e) => {
+    e.preventDevault()
+    const data = {
+      username: e.target.username.value,
+      password: e.target.password.value
+    }
+    const endpoint = 'https://locahost:5000/login'
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    const response = await fetch(endpoint, options)
+    const result = await response.json()
+  }
+
+  return (
      <div>
        <Head>
             <title>SL2 | Login</title>
        </Head>
-       <main className={LoginStyle.test}>
+       <main>
             <Navbar/>
-            <p>
-                This is login page
-            </p>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input placeholder="username" type="text" id="username" name="username"/>
+              </div>
+              <div>
+                <input placeholder="password" type="password" id="password" name="password"/>
+              </div>
+              <button type='submit'>Login</button>
+            </form>
        </main>
      </div>
    )
