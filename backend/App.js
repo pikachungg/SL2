fa3d50a3b10ec79595543aedb8d91780fc146f1e
pikachungg <pikachungg@gmail.com>
2022-10-18 14:00:03 -0400

    started documentation

--- backend/App.js
+++ backend/App.js
@@ -1,42 +1,51 @@
-const bodyParser = require('body-parser'); 
-const express = require('express')
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-const authetication = require('./routes/Authetication.js')
-const students = require('./routes/Students.js')
-const professors = require('./routes/Professors.js')
+const authetication = require("./routes/Authetication.js");
+const students = require("./routes/Students.js");
+const professors = require("./routes/Professors.js");
 
-app.post('/login', authetication.login);
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+var options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "SL2 | Documentation",
+};
 
-app.get('/students/classid/:classid', students.getStudentsByClassId)
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
-app.get('/students/uid/:uid', students.getStudentsByUID)
+app.post("/login", authetication.login);
 
-// all students
-app.get('/students', students.getAllStudents)
+app.get("/students/classid/:classid", students.getStudentsByClassId);
 
-app.get('/students/courses/', students.getStudentsByCourses)
+app.get("/students/uid/:uid", students.getStudentsByUID);
 
-app.get('/professors/uid/:uid', professors.getProfessorById)
+app.get("/students", students.getAllStudents);
 
-app.get('/professors/courses/:uid', professors.getCoursesByProfessor)
+app.get("/students/courses/", students.getStudentsByCourses);
 
-app.get('/professors/notifications/:professorid', professors.getRecentStudentsNotifications)
+app.get("/professors/uid/:uid", professors.getProfessorById);
 
-app.get('/professors/pinned/:uid', professors.getPinnedStudents)
+app.get("/professors/courses/:uid", professors.getCoursesByProfessor);
 
-app.get('/students/id/:id', students.getStudentsById)
+app.get("/professors/notifications/:professorid", professors.getRecentStudentsNotifications);
 
-app.patch('/professors/pinned', professors.addPinnedStudent)
+app.get("/professors/pinned/:uid", professors.getPinnedStudents);
 
-app.delete('/professors/pinned', professors.removePinnedStudent)
+app.get("/students/id/:id", students.getStudentsById);
+
+app.patch("/professors/pinned", professors.addPinnedStudent);
+
+app.delete("/professors/pinned", professors.removePinnedStudent);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
